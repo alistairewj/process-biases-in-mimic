6ccc0411b4b7f2635e28f1854ca312f4720ceed8
Alistair Johnson <alistairewj@gmail.com>
2022-07-12 13:09:17 -0400

    enforce admission to be during anchor_year

--- acquire/cohort/cohort.sql
+++ acquire/cohort/cohort.sql
@@ -2,7 +2,14 @@ SELECT h.hadm_id
 , CASE WHEN p.gender NOT IN ('M', 'F') THEN 1 ELSE 0 END AS exclusion_unknown_sex
 , CASE WHEN p.anchor_age IS NULL THEN 1 ELSE 0 END AS exclusion_unknown_age
 , CASE WHEN p.anchor_age > 89 THEN 1 ELSE 0 END AS exclusion_older_than_89
-, CASE WHEN p.anchor_year_group NOT IN ('2011 - 2013') THEN 1 ELSE 0 END AS exclusion_outside_cohort
+, CASE
+    -- first admission 2008 - 2010, and the current one is at least 3 years later
+    WHEN p.anchor_year_group IN ('2008 - 2010')
+        AND p.anchor_year < (EXTRACT(YEAR FROM h.dischtime) + 3)
+        AND EXTRACT(YEAR FROM h.dischtime) < (p.anchor_year + 6)
+    WHEN p.anchor_year_group IN ('2011 - 2013')
+        AND p.anchor_year < (EXTRACT(YEAR FROM h.dischtime) + 3)
+    THEN 0 ELSE 1 END AS exclusion_outside_cohort
 FROM `physionet-data.mimiciv_hosp.admissions` h
 INNER JOIN `physionet-data.mimiciv_hosp.patients` p
     ON h.subject_id = p.subject_id
